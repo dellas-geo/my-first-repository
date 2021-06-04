16f4de473267723173371f1642d2c06b9e6febfe
dellas-geo <geordellas@gmail.com>
2021-06-04 19:29:02 +0200

    JS-Practice-GFA

---
+++ JavaScript-Practice-GFA/Exercise-2/scripts.js
@@ -0,0 +1,23 @@
+// Part 1
+
+let age = 29;
+let ww2 = 6;
+let tesla = 50000;
+let secondsOfDay = 86400;
+let hungarians = 0.12%;
+let bigger = 12%;
+let participants = 3;
+
+// Part 2
+
+let names [ "Ioanna", "Maria", "Nikos" ]
+console.log(names.lenght);
+names.push('George');
+console.log(names.lenght);
+
+
+// Part 3
+
+let object = {
+    colors: "Red", "Black", "Green",
+    hasManyColors: if (colors > 3)  {"True"};
