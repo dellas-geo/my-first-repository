fb8537d420bb9eb9782ad3f1ce7f0a25c89e8089
dellas-geo <geordellas@gmail.com>
2021-06-06 00:24:26 +0200

    JS-Exercise-5

---
+++ JavaScript-Practice-GFA/Exercise-5/Ex-1/script.js
@@ -0,0 +1,3 @@
+$('#box-1').text('Marcotis')
+$('#box-2').css('border-color', 'purple')
+$('p').css('background', ('yellow'))
---
+++ JavaScript-Practice-GFA/Exercise-5/Ex-2/script.js
@@ -0,0 +1,21 @@
+let color = ('purple');
+let number = 10;
+let word = ('cool');
+
+if (color= 'purple') 
+{$('.red').css('background', 'purple')}
+
+$('.yellow').text('Right')
+
+if (number > 100) 
+{$('.yellow').text('Woah, thats a big number.')} 
+else
+{$('.yellow').text('just a regular number, please.')}
+
+
+if (word='cool') 
+{$('.green').text('Power of DOM')}
+else
+{$('.blue').text('Power of DOM')}
+
+
