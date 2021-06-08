cc2f06e3d8d82673b69a757b0a808cff6599b822
dellas-geo <geordellas@gmail.com>
2021-06-08 17:38:05 +0200

    JS-Exercise-8

--- JavaScript-Practice-GFA/Exercise-7/script.js
+++ JavaScript-Practice-GFA/Exercise-7/script.js
@@ -1,3 +1,4 @@
 $('#1').on ('click', () => {console.log('Yeah, you clicked me')})
 $('#2').on ('click', () => { $('#1').text('TaDA!')})
 $('#3').on ('click', () => { $('button').css('background-color', 'green')})
+
